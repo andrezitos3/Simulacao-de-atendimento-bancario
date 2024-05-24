35c126ee39abaf515e40add8d743d33e55b7921d
Andre Lui Tornice <65464498+andrezitos3@users.noreply.github.com>
2024-05-24 17:39:14 -0300

    trabalhando na lógica de tempo de espera de um guichê

--- SimulacaoDeAtendimentoBancario.java
+++ SimulacaoDeAtendimentoBancario.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.simulacaodeatendimentobancario;
 
 import java.util.ArrayList;
@@ -9,46 +5,48 @@ import java.io.*;
 import java.lang.Thread;
 import java.util.Random;
 
-
 public class SimulacaoDeAtendimentoBancario {
-    
-    static int segundosContados;
-    
+
+    static int segundosContados = 0;
+
     public static void main(String[] args) throws InterruptedException {
-        
         ArrayList<Integer> Fila = new ArrayList<Integer>();
         Random rd = new Random();
         Guiche guiche1 = new Guiche();
 
-        //Adiciona cliente na fila
-            
-           // inicializar cliente
-           int cliente = 123;
-           
-           // Adiciona
-           while(segundosContados < 21600) {
+        // inicializa cliente
+        int Cliente = 123;
+        int finall = 0;
 
-            cliente = rd.nextInt(0, 30);
+
+        while(segundosContados < 21600) {
+
+            // Adiciona cliente a fila
+            Cliente = rd.nextInt(0, 30);
             segundosContados++;
 
-            if(cliente == 0) {
-                
+            if(Cliente == 0) {
                 Fila.add(segundosContados);
-                 System.out.println("Cliente encontrado na fila no segundo " + segundosContados);
-                 
-                if (guiche1.ocupado == false){
-                    guiche1.utilizando(Fila);
-                    int opcao = guiche1.operacao();
-                    int tempoSaida = segundosContados + opcao;
-                    System.out.println("operacao feita em segundos: " + opcao);
+                System.out.println("Cliente encontrado na fila no segundo " + segundosContados);
+
+                if(guiche1.ocupado == false) {
+                    int op = guiche1.operacao();
+                    System.out.println("operacao: " + op);
+                    finall = segundosContados + op;
+                    System.out.println("Segundos contados: " + segundosContados);
+                    System.out.println("tempo que o cliente vai sair do guiche: " + finall);
+                    guiche1.ocupado = true;
+                }
+                if(segundosContados >= finall){
                     guiche1.ocupado = false;
-                    System.out.println("tempo de saida " + tempoSaida);
+                    System.out.println("Segundos contados: " + segundosContados);
+                    System.out.println("Liberado!!!");
                 }
             }
 
         }
-        
-        System.out.println(Fila.size());
-        
-  }
+        System.out.println(Fila);
+        guiche1.utilizando(Fila);
+        System.out.println(Fila);
+    }
 }
